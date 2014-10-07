8a38953026c0c8ecdbea2069cdbcb3a0035393a1
matmunk <matmunk@gmail.com>
2014-10-07 15:32:15 +0200

    Added more items in sidebar + new icons.

--- src/site.js
+++ src/site.js
@@ -1,17 +1,39 @@
 $("#sidebar-header").click(function() {
     var page = $("#page-wrapper");
     var sidebar = $("#sidebar-wrapper");
+    var footer = $("#sidebar-footer");
 
     if (sidebar.css("left") == "0px") {
         sidebar.css("left", "-184px");
         page.css("padding-left", "66px");
-        $("#sidebar-footer").hide();
-    }
-    else {
+        footer.hide();
+        $(".sidebar-submenu").hide();
+    } else {
         sidebar.css("left", "0px");
         page.css("padding-left", "250px");
-        $("#sidebar-footer").show();
+        footer.show();
     }
+});
+
+$("#save").click(function() {
+    if (typeof(Storage) !== "undefined") {
+        var title = $(".project-title").text();
+        var desc = $(".project-desc").text();
+        var ccs = ace.edit("content").getSession().getValue();
+        var key = title.replace(/\s+/g, '');
+        var value = {
+            title: title,
+            desc: desc,
+            ccs: ccs
+        }
+        localStorage["projects/" + key] = value;
+    } else {
+        alert("Your browser does not support Web Storage.");
+    }
+});
+
+$("#load").click(function() {
+    $(".sidebar-submenu").toggle();
 });
 
 $("document").ready(function() {
