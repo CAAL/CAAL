ae467c1145f4149777ed4d020582d068d180181d
Mathias Munk Hansen <matmunk@gmail.com>
2014-11-11 19:12:27 +0100

    Add bottom margin to activities

--- src/activity/explorer.ts
+++ src/activity/explorer.ts
@@ -167,7 +167,9 @@ module Activity {
 
         private resize(): void {
             var width = this.canvas.parentNode.clientWidth;
-            var height = Math.max(400, window.innerHeight - $("#arbor-canvas").offset().top - $(this.statusTableContainer).height());
+            var offsetTop = $("#arbor-canvas").offset().top;
+            var offsetBottom = $(this.statusTableContainer).height() + 20; // Parent container margin = 20.
+            var height = Math.max(400, window.innerHeight - offsetTop - offsetBottom);
             this.canvas.width = width;
             this.canvas.height = height;
             this.renderer.resize(width, height);
