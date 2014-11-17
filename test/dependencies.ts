b1bd534985c1df26ffb2ed4c078e3de7bdd74617
Soren Enevoldsen <senevoldsen90@gmail.com>
2014-11-17 10:17:14 +0100

    Main.js run by index.html when ready

--- test/dependencies.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../src/ccs/ccs.ts" />
-/// <reference path="../src/ccs/depgraph.ts" />
-/// <reference path="../src/ccs/reducedparsetree.ts" />
-/// <reference path="../src/ccs/hml.ts" />
-
-
-/*** This file is used at the moment since directly running code is performed in lib/main.js
-    like referring to document, which fails the tests ***/
