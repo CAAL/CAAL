a8105620e3f378d27c43cdc4859884876f239db3
Mathias Munk Hansen <matmunk@gmail.com>
2015-02-04 02:12:40 +0100

    Remove hover from Project button

--- src/activity/activity.ts
+++ src/activity/activity.ts
@@ -1,9 +1,8 @@
-
 module Activity {
    export class Activity {
-        public beforeShow(configuration) : void {}
-        public afterShow() : void {}
-        public beforeHide() : void {}
-        public afterHide() : void {}
-    } 
+        public beforeShow(configuration): void {}
+        public afterShow(): void {}
+        public beforeHide(): void {}
+        public afterHide(): void {}
+    }
 }
