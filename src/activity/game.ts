abd005616682914532ecf43dc1da1fa41424e731
matmunk <matmunk@gmail.com>
2015-02-19 09:04:48 +0100

    Temp

--- src/activity/game.ts
+++ src/activity/game.ts
@@ -6,52 +6,151 @@ module Activity {
 
     export class Game extends Activity {
         private project : Project;
-        private graph : ccs.Graph;
-        private succGen : ccs.SuccessorGenerator;
-        private leftTree : D3.Layout.TreeLayout;
-        private rightTree : D3.Layout.TreeLayout;
+        private graph : CCS.Graph;
+        private succGen : CCS.SuccessorGenerator;
+        private $gameType : JQuery;
+        private $leftProcessList : JQuery;
+        private $rightProcessList : JQuery;
+        private leftSvg : D3.Selection;
+        private rightSvg : D3.Selection;
 
         constructor(container : string, button : string) {
             super(container, button);
 
             this.project = Project.getInstance();
-            this.leftTree = d3.layout.tree().size([200, 200]);
-            this.rightTree = d3.layout.tree().size([200, 200]);
-            var diagonal = d3.svg.diagonal()
-                           .projection(function(d) { return [d.x, d.y]; });
-        }
 
-        protected checkPreconditions(): boolean {
-            this.graph = Main.getGraph();
+            this.$gameType = $("#game-type");
+            this.$leftProcessList = $("#game-left-process");
+            this.$rightProcessList = $("#game-right-process");
 
-            if (!this.graph) {
-                this.showExplainDialog("Syntax Error", "Your program contains syntax errors.");
-                return false;
-            }
+            this.$gameType.add(this.$leftProcessList).add(this.$rightProcessList).on("change", () => this.newGame());
 
-            if (this.graph.getNamedProcesses().length === 0) {
-                this.showExplainDialog("No Named Processes", "There must be at least one named process in the program to explore.");
-                return false;
-            }
-
-            return true;
+            this.leftSvg = d3.select("#game-left-svg").append("svg")
+                .attr("width", "100%");
+            this.rightSvg = d3.select("#game-right-svg").append("svg")
+                .attr("width", "100%");
         }
 
         public onShow(configuration? : any) : void {
-            //if (this.project.getChanged()) {
-                var graph = this.project.getGraph();
-                CCSParser.parse(Project.getInstance().getCCS(), {ccs: CCS, graph: graph});
-                this.succGen = CCS.getSuccGenerator(graph, {succGen: "strong", reduce: false});
-                console.log(this.succGen.getSuccessors(graph.processByName("Peterson").id).toArray());
-            //}
+            $(window).on("resize", () => this.resize());
+            this.resize();
+
+            if (this.project.getChanged()) {
+                this.graph = this.project.getGraph();
+                this.displayOptions();
+                this.newGame();
+            }
         }
 
         public onHide() : void {
-            
+            $(window).off("resize");
         }
 
-        private draw() : void {
+        private displayOptions() : void {
+            var processes = this.graph.getNamedProcesses().reverse();
+            
+            this.$leftProcessList.empty();
+            this.$rightProcessList.empty();
 
+            for (var i = 0; i < processes.length; i++) {
+                this.$leftProcessList.append($("<option></option>").append(processes[i]));
+                this.$rightProcessList.append($("<option></option>").append(processes[i]));
+            }
+
+            // Set second option as default selection for the right process.
+            this.$rightProcessList.find("option:nth-child(2)").prop("selected", true);
+        }
+
+        private getOptions() : any {           
+            return {
+                gameType: this.$gameType.val(),
+                leftProcess: this.$leftProcessList.val(),
+                rightProcess: this.$rightProcessList.val()
+            };
+        }
+
+        private newGame() : void {
+            var options = this.getOptions();
+            this.succGen = CCS.getSuccGenerator(this.graph, {succGen: options.gameType, reduce: false});
+            this.draw(this.leftSvg);
+        }
+
+        private draw(svg : D3.Selection) : void {
+            var graph = this.getGraph(this.graph.processByName("A"));
+
+            var test = [1,2,3,4,5];
+            test[10] = 2
+            console.log(test);
+
+            var nodes = graph.nodes;
+            var links = graph.links;
+            
+            console.log(graph.nodes);
+            console.log(graph.links);
+
+            var force = d3.layout.force()
+                .nodes(nodes)
+                .links(links)
+                .size([parseInt(svg.attr("width")), parseInt(svg.attr("height"))]);
+
+            force.start();
+            for (var i = 100; i > 0; --i) force.tick();
+            force.stop();
+
+            svg.selectAll("circle")
+                .data(nodes)
+                .enter().append("circle")
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; })
+                .attr("r", 4.5);
+
+            svg.selectAll("line")
+                .data(links)
+                .enter().append("line")
+                .attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+        }
+
+        private getNodes() : any[] {
+            return this.graph.getNamedProcesses().map(function(p) {return {name: p}});
+        }
+
+        private getGraph(start : CCS.Process) : any {
+            var nodes = [],
+                links = [],
+                waiting = [start],
+                done = [],
+                source;
+
+            for (var i = 0; i < waiting.length; i++) {
+                source = waiting[i];
+                done.push(source.id);
+
+                this.succGen.getSuccessors(source.id).forEach(t => {
+                    if (done.indexOf(t.targetProcess.id) === -1) {
+                        waiting.push(t.targetProcess);
+                    }
+
+                    nodes[source.id] = {id: source.id, name: source.name};
+                    links.push({source: source.id, target: t.targetProcess.id});
+                });
+            }
+
+            return {nodes: nodes, links: links};
+        }
+
+        private resize() : void {
+            var offsetTop = $("#game-main").offset().top;
+            var offsetBottom = $("#game-log").height();
+
+            // Height = Total - (menu + options) - log - (margin + border).
+            // Minimum size 275 px.
+            var height = Math.max(275, window.innerHeight - offsetTop - offsetBottom - 41);
+
+            this.leftSvg.attr("height", height);
+            this.rightSvg.attr("height", height);
         }
     }
 
