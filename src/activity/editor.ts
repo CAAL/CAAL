431ae157231ca98ea4312ff227170598c725f9c4
Mathias Munk Hansen <matmunk@gmail.com>
2015-04-18 03:55:56 +0200

    Error popovers now sticks

--- src/activity/editor.ts
+++ src/activity/editor.ts
@@ -44,7 +44,7 @@ module Activity {
             }
 
             this.$parse.on("click", () => this.parse());
-            this.$parse.popover({delay: {"show": 100, "hide": 100}, html: true, placement: "bottom", trigger: "focus"});
+            this.$parse.popover({delay: {"show": 100, "hide": 100}, html: true, placement: "bottom", trigger: "manual"});
 
             $("#input-mode").on("change", (e) => this.setInputMode(e));
             $("#font-size").on("change", (e) => this.setFontSize(e));
@@ -73,7 +73,7 @@ module Activity {
         private parse() : void {
             var graph, errors, title, content;
 
-            title = "<span class=\"text-danger\"><i class=\"fa fa-exclamation-circle fa-lg\"></i> Error</span>";
+            title = '<span class="text-danger"><i class="fa fa-exclamation-circle fa-lg"></i> Error</span>';
 
             try {
                 graph = this.project.getGraph();
@@ -85,15 +85,42 @@ module Activity {
                         content += "<p>" + errors[i].name + ": " + errors[i].message + "</p>";
                     }
                 } else {
-                    title = "<span class=\"text-success\"><i class=\"fa fa-check fa-lg\"></i> Success</span>";
+                    title = '<span class="text-success"><i class="fa fa-check fa-lg"></i> Success</span>';
                     content = "<p>The program is valid " + InputMode[this.project.getInputMode()] + ".</p>";
+                    this.showPopover(title, content, false);
+                    return;
                 }
             } catch (error) {
                 content = "<p>" + error.name + ": " + error.message + "</p>";
             }
 
+            this.showPopover(title, content, true);
+        }
+
+        private showPopover(title : string, content : string, sticky : boolean) : void {
             this.$parse.attr("data-original-title", title);
             this.$parse.attr("data-content", content);
+            this.$parse.popover("show");
+
+            if (sticky) {
+                this.$parse.siblings().find(".popover-title").append('<button type="button" id="parse-close" class="close">&times;</button>');
+
+                $(document).off("click", this.handleClick);
+
+                $("#parse-close").on("click", () => {
+                    this.$parse.popover("hide");
+                    this.editor.focus();
+                });
+            } else {
+                $(document).on("click", this.handleClick);
+            }
+        }
+
+        // http://stackoverflow.com/questions/11703093/
+        private handleClick = (e) => {
+            if (!$(e.target).is(this.$parse) && $(e.target).parents(".popover.in").length === 0) { 
+                this.$parse.popover("hide");
+            }
         }
 
         private setInputMode(e) : void {
