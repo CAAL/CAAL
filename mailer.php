6e6fc68c3e2d14d920a35c79e2dd7d8ddbabd11a
Nicklas Andersen <figa.figa@gmail.com>
2015-06-15 11:23:50 +0200

    mailer php

--- mailer.php
+++ mailer.php
@@ -4,10 +4,19 @@ require("mailconfig.php");
 
 global $mailconfig;
 
-if(mail($mailconfig->to, $_POST["contact-cat-subject"], $_POST["contact-cat-text"], $mailconfig->headers)) {
-    echo "true";
+if($_POST["contact-cat-email"] !== "") {
+    $headers = 'From: '.$_POST["contact-cat-email"]."\r\n".'Reply-To: '.$_POST["contact-cat-email"];
+    if(mail($mailconfig->to, $_POST["contact-cat-subject"], $_POST["contact-cat-text"], $headers)) {
+        echo "true";
+    } else {
+        echo "false";
+    }
 } else {
-    echo "false";
+    if(mail($mailconfig->to, $_POST["contact-cat-subject"], $_POST["contact-cat-text"])) {
+        echo "true";
+    } else {
+        echo "false";
+    }
 }
     
 ?>
