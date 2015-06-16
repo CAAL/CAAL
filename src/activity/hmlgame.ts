77682f196ac1a7aba78789b6d4e8c1f353b8cf8b
Jacob Wortmann <jacob@wortmann.dk>
2015-06-16 15:45:16 +0200

    gamelog fix requested by Jiri

--- src/activity/hmlgame.ts
+++ src/activity/hmlgame.ts
@@ -412,9 +412,11 @@ module Activity {
                 }
 
                 case WinReason.stuck: {
-                    gameLogObject.setTemplate("No available transitions. You ({0}) {1}!");
-                    gameLogObject.addLabel({text: (this.human === Player.defender) ? "defender" : "attacker"});
-                    gameLogObject.addLabel({text: (this.human === winner) ? "win" : "lose"});
+                    gameLogObject.setTemplate("{0} {1} no available transitions. You lose!");
+                    gameLogObject.addLabel({text: (this.human === winner) ? (this.computer === Player.defender ? "Defender" : "Attacker") : 
+                                            "You " +"("+ (this.human === Player.defender ? "defender" : "attacker") + ")"});
+                    gameLogObject.addLabel({text: (this.human === winner) ? "has" : "have"});
+                    gameLogObject.addLabel({text: (this.human === winner) ? "You" : (this.computer === Player.defender ? "defender" : "attacker")});
                     break;
                 }
                 default: {
