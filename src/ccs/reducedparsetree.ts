48badf7182461a511c76f13d39b80e98cfd0554f
Søren Enevoldsen <senevoldsen90@gmail.com>
2015-06-19 21:24:07 +0200

    Rewrote strict paths in abstract succ gen

--- src/ccs/reducedparsetree.ts
+++ src/ccs/reducedparsetree.ts
@@ -157,101 +157,70 @@ module Traverse {
             return this.strictSuccGenerator.getProcessById(processId);
         }
 
-        getSuccessors(processId : ccs.ProcessId) : ccs.TransitionSet {
-            if (this.cache[processId]) return this.cache[processId];
+        getSuccessors(sourceProcessId : ccs.ProcessId) : ccs.TransitionSet {
+            if (this.cache[sourceProcessId]) return this.cache[sourceProcessId];
 
-            //Manuall add tau loop to from set:  P ==tau=> P, by P -- tau -> P
-            this.abstractions.forEach(abstraction => {
-                var sourceFullTransition = new FullTransition(processId, abstraction, processId);
-                if (!this.fromTable.has(sourceFullTransition)) {
-                    var sourceFromData = new FromData(null, abstraction, processId);
-                    this.fromTable.set(sourceFullTransition, sourceFromData);
-                }
-            });
+            var result = new ccs.TransitionSet();
+            var sourceProcess = this.strictSuccGenerator.getProcessById(sourceProcessId);
+            var stage1Processes = [];
+            var stage2Processes = [];
 
-            var result = new ccs.TransitionSet(),
-                process = this.strictSuccGenerator.getProcessById(processId),
-                toVisitProcesses = [process],
-                visitedStage1 = {},
-                toVisitStage2Processes = [],
-                toVisitStage2Actions = [],
-                visitedStage2 = {},
-                visitingProcess,
-                visitingAction,
-                strongSuccessors;
-            
-            this.abstractions.forEach(abstraction => {
-                //Add  P --tau--> P
-                result.add(new ccs.Transition(abstraction, process));
-            });
-            
+            var addTransitions = (array : any[], prevFromData : FromData, fromProcessId : ccs.ProcessId, isStageTwo : boolean) => {
+                var strongSuccessors = this.strictSuccGenerator.getSuccessors(fromProcessId);
+                this.abstractions.forEach(abstraction => {
+                    strongSuccessors.forEach(transition => {
+                        if (!isStageTwo || transition.action.equals(abstraction)) {
+                            //If in stage two then reuse the first non-abstract action that was taken.
+                            //Afterwards all non-abstractions after the first can be inferred to really be abstractions.
+                            var action = isStageTwo ? prevFromData.action : transition.action;
+                            array.push(new FromData(prevFromData, action, transition.targetProcess.id));
+                            result.add(new CCS.Transition(action, transition.targetProcess));
+                        }
+                    });
+                });
+            };
+
+            addTransitions(stage1Processes, null, sourceProcessId, false);
+
             //Stage 1
             //Find all --tau-->* and
             //     all --tau-->*  --x-->
-            while (toVisitProcesses.length > 0) {
-                visitingProcess = toVisitProcesses.pop();
-                if (!visitedStage1[visitingProcess.id]) {
-                    visitedStage1[visitingProcess.id] = true;
-                    var prevFromDatas = [];
-                    this.abstractions.forEach(abstraction => {
-                        prevFromDatas.push(this.fromTable.get(new FullTransition(processId, abstraction, visitingProcess.id)));
-                    });
-                    strongSuccessors = this.strictSuccGenerator.getSuccessors(visitingProcess.id);
-                    strongSuccessors.forEach(transition => {
-
-                        //Remember how we got to P => Q to be able to get strict path P -> P1 -> P2 -> Q later.
-                        var fullTransition = new FullTransition(processId, transition.action, transition.targetProcess.id);
-                        if (!this.fromTable.has(fullTransition)) {
-                            prevFromDatas.forEach(prevFromData => {
-                                var fromData = new FromData(prevFromData, transition.action, transition.targetProcess.id);
-                                this.fromTable.set(fullTransition, fromData);
-                            });
-                        }
-                        
-                        if (this.abstractions.some(abstraction => transition.action.getLabel() === abstraction.getLabel())) {
-                            this.abstractions.forEach(abstraction => {
-                                result.add(new CCS.Transition(abstraction.clone(), transition.targetProcess));  // --tau--> x
-                            });
-                            toVisitProcesses.push(transition.targetProcess);
-                        } else {
-                            toVisitStage2Processes.push(transition.targetProcess);
-                            toVisitStage2Actions.push(transition.action);
-                            visitedStage2[transition.action] = {};
-                            result.add(transition); // --a--> x
-                        }
-                    });
+            while (stage1Processes.length > 0) {
+                var fromData = stage1Processes.pop();
+                //  P == action ==> Q
+                var fullTransition = new FullTransition(sourceProcessId, fromData.action, fromData.toId);
+                if (!this.fromTable.has(fullTransition)) {
+                    this.fromTable.set(fullTransition, fromData);
+                    if (this.abstractions.some(abstraction => abstraction.equals(fromData.action))) {
+                        addTransitions(stage1Processes, fromData, fromData.toId, false);
+                    } else {
+                        addTransitions(stage2Processes, fromData, fromData.toId, true);
+                    }
                 }
             }
 
             //Stage 2
             //Find all continuing  P --tau-->* when already --tau->* --x--> P
-            while (toVisitStage2Processes.length > 0) {
-                visitingProcess = toVisitStage2Processes.pop();
-                visitingAction = toVisitStage2Actions.pop();
-                var prevFromData = this.fromTable.get(new FullTransition(processId, visitingAction, visitingProcess.id));
-                if (!visitedStage2[visitingAction][visitingProcess.id]) {
-                    visitedStage2[visitingAction][visitingProcess.id] = true;
-                    strongSuccessors = this.strictSuccGenerator.getSuccessors(visitingProcess.id);
-                    strongSuccessors.forEach(transition => {
-                        if (this.abstractions.some(abstraction => transition.action.getLabel() === abstraction.getLabel())) {
-
-                            var fullTransition = new FullTransition(processId, visitingAction, transition.targetProcess.id);
-                            if (!this.fromTable.has(fullTransition)) {
-                                var fromData = new FromData(prevFromData, visitingAction, transition.targetProcess.id);
-                                this.fromTable.set(fullTransition, fromData);
-                            }
-
-                            toVisitStage2Processes.push(transition.targetProcess);
-                            toVisitStage2Actions.push(visitingAction);
-                            var newTransition = new ccs.Transition(visitingAction, transition.targetProcess) 
-                            result.add(newTransition);
-                        }
-                    });
+            while (stage2Processes.length > 0) {
+                var fromData = stage2Processes.pop();
+                var fullTransition = new FullTransition(sourceProcessId, fromData.action, fromData.toId);
+                if (!this.fromTable.has(fullTransition)) {
+                    this.fromTable.set(fullTransition, fromData);
+                    addTransitions(stage2Processes, fromData, fromData.toId, true);
                 }
             }
 
+            //Add tau loop to from set:  P ==tau=> P, by P -- tau -> P
+            this.abstractions.forEach(abstraction => {
+                var fullTransition = new FullTransition(sourceProcessId, abstraction, sourceProcessId);
+                if (!this.fromTable.has(fullTransition)) {
+                    var fromData = new FromData(null, abstraction, sourceProcessId);
+                    this.fromTable.set(fullTransition, fromData);
+                }
+                result.add(new CCS.Transition(abstraction, sourceProcess));
+            });
 
-            this.cache[processId] = result;
+            this.cache[sourceProcessId] = result;
             return result;
         }
 
@@ -265,17 +234,17 @@ module Traverse {
             do {
                 path.push(new ccs.Transition(fromData.action, succGen.getProcessById(fromData.toId)));
                 fromData = fromData.prev;
-            } while (fromData && fromData.toId !== fromId);
+            } while (fromData);
 
             path.reverse();
-            //Fix so only one non-tau
-            var hasNonTau = false;
+            //Fix so only one non-abstraction
+            var hasNonAbstraction = false;
             var result = path.map(transition => {
                 var resultTransition = transition;
-                if (hasNonTau) {
+                if (hasNonAbstraction) {
                     resultTransition = new ccs.Transition(this.abstractions[0], transition.targetProcess);
                 } else if (this.abstractions.every(abstraction => !abstraction.equals(transition.action))) { 
-                    hasNonTau = true;
+                    hasNonAbstraction = true;
                 }
                 return resultTransition;
             });
